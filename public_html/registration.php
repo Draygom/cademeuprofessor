f298a364f53d2b2cb48c5c7c98c68af971f8c976
Pedro Chaves <draygom@gmail.com>
2018-09-04 20:25:25 -0300

    Melhora o cadastro.

--- public_html/registration.php
+++ public_html/registration.php
@@ -11,17 +11,20 @@ include("menu.php");
 		$email = mysqli_real_escape_string($con,$email);
 		$password = stripslashes($_REQUEST['password']);
 		$password = mysqli_real_escape_string($con,$password);
+    $tipo_usuario = stripslashes($_REQUEST['tipo_usuario']);
+    $tipo = mysqli_real_escape_string($con, $tipo_usuario);
 		$data = date("Y-m-d H:i:s");
-        $query = "INSERT into `usuarios` (apelido, nome, password, email, data_cadastro) VALUES ('$apelido', 'nome', '".md5($password)."', '$email', '$data')";
+        $query = "INSERT into `usuarios` (apelido, tipo_usuario_id_tipo_usuario, nome, password, email, data_cadastro) VALUES ('$apelido', '$tipo_usuario', 'nome', '".md5($password)."', '$email', '$data')";
         $result = mysqli_query($con,$query);
         if($result){
             echo "<div class='form'><h3>Cadastro bem sucedido.</h3><br/>Clique aqui para entrar <a href='login.php'>Login</a></div>";
         }
     }else{
 ?>
-<div class="container-fluid">
+<div class="container-fluid col-6">
   <h1 class="display-4 text-center">Cadastro</h1>
   <form name="registration" action="" method="post">
+    <input type="hidden" class="" name="tipo_usuario" value="1" />
     <input type="text" class="form-control my-1" name="apelido" placeholder="Apelido" required />
     <input type="text" class="form-control my-1" name="nome" placeholder="Nome" required />
     <input type="email" class="form-control my-1" name="email" placeholder="E-mail" required />
